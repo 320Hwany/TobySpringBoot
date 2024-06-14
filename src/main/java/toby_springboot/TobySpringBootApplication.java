13a63a703324b0f643b032c8c75f18e4104f086e
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-14 22:28:41 +0900

    feat : 서블릿 컨테이너 띄우기

---
+++ src/main/java/toby_springboot/TobySpringBootApplication.java
@@ -0,0 +1,16 @@
+package toby_springboot;
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+public class TobySpringBootApplication {
+
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+    }
+
+}
---
+++ src/test/java/toby_springboot/TobySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package toby_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
