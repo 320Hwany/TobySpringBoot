06b58494db61ae5b30f37c2edd9187100f5facab
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-14 22:44:50 +0900

    feat : 서블릿 등록

--- src/main/java/toby_springboot/TobySpringBootApplication.java
+++ src/main/java/toby_springboot/TobySpringBootApplication.java
@@ -1,15 +1,32 @@
 package toby_springboot;
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import java.io.IOException;
 
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+
+
+        });
         webServer.start();
     }
 
