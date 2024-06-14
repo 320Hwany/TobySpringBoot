a453d2aa363d74763e883c91203bc75d0df5bcb2
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-14 23:06:57 +0900

    feat : 프론트 컨트롤러로 전환

--- src/main/java/toby_springboot/HelloController.java
+++ src/main/java/toby_springboot/HelloController.java
@@ -1,12 +1,8 @@
 package toby_springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
