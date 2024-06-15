1dc0939ca3d78d23292cf8a7e09da23d6949d556
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-15 14:28:10 +0900

    feat : 자바 코드 구성 정보 사용

--- src/main/java/toby_springboot/TobySpringBootApplication.java
+++ src/main/java/toby_springboot/TobySpringBootApplication.java
@@ -4,14 +4,26 @@ package toby_springboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
+@Configuration
 public class TobySpringBootApplication {
 
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 
             @Override
             protected void onRefresh() {
@@ -26,8 +38,7 @@ public class TobySpringBootApplication {
             }
         };
 
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(TobySpringBootApplication.class);
         applicationContext.refresh();
     }
 }
