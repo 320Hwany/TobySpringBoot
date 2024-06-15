fbeb2bc92190d5123ea818619a28dff2ca512117
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-15 15:24:33 +0900

    feat : MySpringApplication -> SpringApplication으로 변경

--- src/main/java/toby_springboot/TobySpringBootApplication.java
+++ src/main/java/toby_springboot/TobySpringBootApplication.java
@@ -1,13 +1,12 @@
 package toby_springboot;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @ComponentScan
@@ -25,24 +24,6 @@ public class TobySpringBootApplication {
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(TobySpringBootApplication.class);
-        applicationContext.refresh();
+        SpringApplication.run(TobySpringBootApplication.class, args);
     }
 }
