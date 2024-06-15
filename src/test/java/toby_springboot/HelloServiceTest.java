127ee1fabdf8371f57f65d94f722578a03cc7a24
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-15 16:38:47 +0900

    test : DI와 단위 테스트

---
+++ src/test/java/toby_springboot/HelloServiceTest.java
@@ -0,0 +1,16 @@
+package toby_springboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloServiceTest {
+
+    @Test
+    void SimpleHelloService() {
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        String ret = helloService.sayHello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("Hello Test");
+    }
+}
