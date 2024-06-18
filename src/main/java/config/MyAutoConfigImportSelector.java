d923aec07555bbebf4e169bd74a902f3a239a04e
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-18 16:23:53 +0900

    refactor : 자동 구성 정보 파일 분리

--- src/main/java/config/MyAutoConfigImportSelector.java
+++ src/main/java/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,26 @@
 package config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(final ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "config.autoconfig.DispatcherServletConfig",
-                "config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
