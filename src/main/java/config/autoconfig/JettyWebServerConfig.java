f45f9e55b6f20430da81abd6b8e95c5b203e206e
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-19 16:48:24 +0900

    feat : 자동 구성 정보 대체하기

--- src/main/java/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package config.autoconfig;
 
 import config.ConditionalMyOnClass;
 import config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 public class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
