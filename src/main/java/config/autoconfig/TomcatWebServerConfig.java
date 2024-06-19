c2db6e1cbae97b855d19c7097895581f3659da75
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-19 15:14:08 +0900

    feat : @Conditional과 Condition

--- src/main/java/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/config/autoconfig/TomcatWebServerConfig.java
@@ -4,12 +4,25 @@ import config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+
+        @Override
+        public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
